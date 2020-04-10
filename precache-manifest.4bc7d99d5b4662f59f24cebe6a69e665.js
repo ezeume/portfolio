7c0e8eeac39446573ddd50cf3554cb44b35c3b05
Nelson Ezeume <nelsonezeume@yahoo.com>
2020-04-09 23:19:10 -0400

    Updates

---
+++ precache-manifest.4bc7d99d5b4662f59f24cebe6a69e665.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41d7a040ddfee2076e4c2caaba854db4",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8818ce562ae5d7df372e",
+    "url": "/portfolio/static/css/main.409cbe37.chunk.css"
+  },
+  {
+    "revision": "24a8842ca47f395e795e",
+    "url": "/portfolio/static/js/2.56a3e764.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.56a3e764.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8818ce562ae5d7df372e",
+    "url": "/portfolio/static/js/main.f07a6fe6.chunk.js"
+  },
+  {
+    "revision": "1e74db4740f99c68a0bf",
+    "url": "/portfolio/static/js/runtime-main.b37861ed.js"
+  },
+  {
+    "revision": "cd33cf27412439382fbc0b790987b224",
+    "url": "/portfolio/static/media/burger.cd33cf27.png"
+  },
+  {
+    "revision": "083bbc8455113a8b1b1fd269dc518524",
+    "url": "/portfolio/static/media/carApp.083bbc84.jpg"
+  },
+  {
+    "revision": "07d6256ad9297deec4806a1f86a91862",
+    "url": "/portfolio/static/media/empdirectory.07d6256a.jpg"
+  },
+  {
+    "revision": "382779d1a66b40b3455ca537ae94e4b5",
+    "url": "/portfolio/static/media/foodfinder.382779d1.jpg"
+  },
+  {
+    "revision": "9310a3ed9f5b62805a0022511066ee63",
+    "url": "/portfolio/static/media/me.9310a3ed.jpg"
+  },
+  {
+    "revision": "97aeb2610ae70fe0bbd75fda30a06a4d",
+    "url": "/portfolio/static/media/team-gen.97aeb261.jpg"
+  },
+  {
+    "revision": "1ca9d1adaf27d97aea88540248fd3533",
+    "url": "/portfolio/static/media/workout.1ca9d1ad.jpg"
+  }
+]);
